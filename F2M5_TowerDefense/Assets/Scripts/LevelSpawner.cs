699719e60224648857138a8ca60cbae54f91c4c8
Richard Ram <richard@richard-ram.com>
2021-04-22 22:51:46 +0200

    Added a basic levelspawner based on json data

--- F2M5_TowerDefense/Assets/Scripts/LevelSpawner.cs
+++ F2M5_TowerDefense/Assets/Scripts/LevelSpawner.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextAsset _TestLevelJson;
+    [SerializeField] private Tile[] _TilePrefabs;
+    
+    private GameObject _levelGameObject;
+    
+    public struct GameData
     {
-        
+        public LevelData[] LevelData;
+    }
+    [System.Serializable]
+    public struct LevelData
+    {
+        public int Width;
+        public int Height;
+        public string[] Tiles;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
+        GameData data = JsonUtility.FromJson<GameData>(_TestLevelJson.ToString());
+        SpawnLevel(data.LevelData[0]);
+    }
+
+    public void SpawnLevel(LevelData levelData)
+    {
+        _levelGameObject = new GameObject("Level");
+        _levelGameObject.transform.SetParent(transform);
         
+        for (int i = 0; i < levelData.Tiles.Length; i++)
+        {
+            int tileTypeIndex = GetTileTypeIndex(levelData.Tiles[i]);
+
+            Tile tile = Instantiate(_TilePrefabs[tileTypeIndex], _levelGameObject.transform);
+            Vector3 tilePosition = new Vector3(i % levelData.Width, 0, i / levelData.Width);
+            tile.name = levelData.Tiles[i];
+            tile.transform.position = tilePosition;
+        }
+    }
+
+    private int GetTileTypeIndex(string tileName)
+    {
+        int index = 0;
+        
+        switch (tileName)
+        {
+            case "Normal":
+                index = 0;
+                break;
+            case "Waypoint":
+                index = 1;
+                break;
+        }
+
+        return index;
     }
 }
