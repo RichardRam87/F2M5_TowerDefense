555046c8e6a8da73da662cea7a983840fe33025d
Richard Ram <richard@richard-ram.com>
2021-04-25 17:47:38 +0200

    cleaned the stop and start op build routine

--- F2M5_TowerDefense/Assets/Scripts/Towers/TowerSpawner.cs
+++ F2M5_TowerDefense/Assets/Scripts/Towers/TowerSpawner.cs
@@ -19,6 +19,8 @@ public class TowerSpawner : MonoBehaviour
     public void SelectTower(int index)
     {
         _selectedTower = _towers[index];
+        
+        StopCoroutine(BuildRoutine());
         StartCoroutine(BuildRoutine());
     }
 
